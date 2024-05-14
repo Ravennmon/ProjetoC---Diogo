1fc890bcfe78553a2d0fa3bf954df3205ae8d9ba
Daniel Pereira Gonçalves <danielpgo11@gmail.com>
2024-05-14 20:26:11 -0300

    fixed relationships

--- back/Models/Servico.cs
+++ back/Models/Servico.cs
@@ -3,12 +3,19 @@ namespace back.Models;
 
 public class Servico
 {
-    public Servico( string nome, string descricao)  {
+    public Servico(string nome, string descricao)
+    {
         Nome = nome;
         Descricao = descricao;
     }
-    
+
     public int Id { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
+
+    public int? OngId { get; set; }
+    public Ong Ong { get; set; }
+
+    public int? CuidadorId { get; set; }
+    public Cuidador Cuidador { get; set; }
 }
--- back/Program.cs
+++ back/Program.cs
@@ -1,23 +1,23 @@
 using back.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>();
 
-builder.Services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
 List<Servico> servicos = new List<Servico>();
 
-app.MapPost("/ongs", ([FromBody] Ong ong, [FromServices] AppDbContext context) => 
+app.MapPost("/ongs", ([FromBody] Ong ong, [FromServices] AppDbContext context) =>
 {
     Ong? ongExistente = context.Ongs.FirstOrDefault(o => o.Nome == ong.Nome);
 
-    if(ongExistente is null)
+    if (ongExistente is null)
     {
         ong.Nome = ong.Nome.ToUpper();
         context.Ongs.Add(ong);
@@ -28,7 +28,7 @@ app.MapPost("/ongs", ([FromBody] Ong ong, [FromServices] AppDbContext context) =
     return Results.BadRequest("Ong já cadastrada");
 });
 
-app.MapGet("/ongs", ([FromServices] AppDbContext context) => 
+app.MapGet("/ongs", ([FromServices] AppDbContext context) =>
 {
     if (context.Ongs.Any())
     {
@@ -38,21 +38,23 @@ app.MapGet("/ongs", ([FromServices] AppDbContext context) =>
     return Results.NotFound("Não existem ongs na tabela");
 });
 
-app.MapGet("/ongs/{id}", ([FromServices] AppDbContext context, int id) => {
-   Ong? ongBuascar = context.Ongs.FirstOrDefault(ong => ong.Id == id);
-    
+app.MapGet("/ongs/{id}", ([FromServices] AppDbContext context, int id) =>
+{
+    Ong? ongBuascar = context.Ongs.FirstOrDefault(ong => ong.Id == id);
+
     if (ongBuascar is null)
     {
         return Results.NotFound("Ong não encontrada");
     }
 
     return Results.Ok(ongBuascar);
-   
+
 });
 
-app.MapDelete("ongs/{id}", ([FromServices] AppDbContext context, int id) => {
-   Ong? ongBuascar = context.Ongs.Find(id);
-    
+app.MapDelete("ongs/{id}", ([FromServices] AppDbContext context, int id) =>
+{
+    Ong? ongBuascar = context.Ongs.Find(id);
+
     if (ongBuascar is null)
     {
         return Results.NotFound("Ong não encontrada");
@@ -61,12 +63,13 @@ app.MapDelete("ongs/{id}", ([FromServices] AppDbContext context, int id) => {
     context.Ongs.Remove(ongBuascar);
     context.SaveChanges();
     return Results.Ok(ongBuascar);
-   
+
 });
 
-app.MapPut("ongs/{id}", ([FromBody] Ong ong, [FromServices] AppDbContext context, int id) => {
+app.MapPut("ongs/{id}", ([FromBody] Ong ong, [FromServices] AppDbContext context, int id) =>
+{
     Ong? ongBuascar = context.Ongs.Find(id);
-    
+
     if (ongBuascar is null)
     {
         return Results.NotFound("Ong não encontrada");
@@ -81,11 +84,11 @@ app.MapPut("ongs/{id}", ([FromBody] Ong ong, [FromServices] AppDbContext context
     return Results.Ok(ongBuascar);
 });
 
-app.MapPost("/cuidadores", ([FromBody] Cuidador cuidador, [FromServices] AppDbContext context) => 
+app.MapPost("/cuidadores", ([FromBody] Cuidador cuidador, [FromServices] AppDbContext context) =>
 {
     Cuidador? cuidadorExistente = context.Cuidadores.FirstOrDefault(c => c.Nome == cuidador.Nome);
 
-    if(cuidadorExistente is null)
+    if (cuidadorExistente is null)
     {
         context.Cuidadores.Add(cuidador);
         context.SaveChanges();
@@ -95,7 +98,7 @@ app.MapPost("/cuidadores", ([FromBody] Cuidador cuidador, [FromServices] AppDbCo
     return Results.BadRequest("Cuidador já cadastrado");
 });
 
-app.MapGet("/cuidadores", ([FromServices] AppDbContext context) => 
+app.MapGet("/cuidadores", ([FromServices] AppDbContext context) =>
 {
     if (context.Cuidadores.Any())
     {
@@ -105,23 +108,25 @@ app.MapGet("/cuidadores", ([FromServices] AppDbContext context) =>
     return Results.NotFound("Não existem cuidadores na tabela");
 });
 
-app.MapGet("/cuidadores/{id}", ([FromServices] AppDbContext context, int id) => {
-   Cuidador? cuidador = context.Cuidadores.Include(p => p.Animais).FirstOrDefault(cuidador => cuidador.Id == id);
-    
+app.MapGet("/cuidadores/{id}", ([FromServices] AppDbContext context, int id) =>
+{
+    Cuidador? cuidador = context.Cuidadores.Include(p => p.Animais).FirstOrDefault(cuidador => cuidador.Id == id);
+
     if (cuidador is null)
     {
         return Results.NotFound("Cuidador não encontrado");
     }
 
-    
+
 
     return Results.Ok(cuidador);
-   
+
 });
 
-app.MapDelete("cuidadores/{id}", ([FromServices] AppDbContext context, int id) => {
-   Cuidador? cuidador = context.Cuidadores.Find(id);
-    
+app.MapDelete("cuidadores/{id}", ([FromServices] AppDbContext context, int id) =>
+{
+    Cuidador? cuidador = context.Cuidadores.Find(id);
+
     if (cuidador is null)
     {
         return Results.NotFound("Cuidador não encontrado");
@@ -130,12 +135,13 @@ app.MapDelete("cuidadores/{id}", ([FromServices] AppDbContext context, int id) =
     context.Cuidadores.Remove(cuidador);
     context.SaveChanges();
     return Results.Ok(cuidador);
-   
+
 });
 
-app.MapPut("cuidadores/{id}", ([FromBody] Cuidador cuidadorRequest, [FromServices] AppDbContext context, int id) => {
+app.MapPut("cuidadores/{id}", ([FromBody] Cuidador cuidadorRequest, [FromServices] AppDbContext context, int id) =>
+{
     Cuidador? cuidador = context.Cuidadores.Find(id);
-    
+
     if (cuidador is null)
     {
         return Results.NotFound("Cuidador não encontrado");
@@ -151,7 +157,8 @@ app.MapPut("cuidadores/{id}", ([FromBody] Cuidador cuidadorRequest, [FromService
     return Results.Ok(cuidador);
 });
 
-app.MapPost("cuidadores/{id}/animal", ([FromBody] Animal animal, [FromServices] AppDbContext context, int id) => {
+app.MapPost("cuidadores/{id}/animal", ([FromBody] Animal animal, [FromServices] AppDbContext context, int id) =>
+{
     Cuidador? cuidador = context.Cuidadores.Find(id);
 
     if (cuidador is null)
@@ -165,7 +172,8 @@ app.MapPost("cuidadores/{id}/animal", ([FromBody] Animal animal, [FromServices] 
     return Results.Ok(context.Animais.ToList());
 });
 
-app.MapPost("ongs/{id}/animais", ([FromBody] Animal animal, [FromServices] AppDbContext context, int id) => {
+app.MapPost("ongs/{id}/animais", ([FromBody] Animal animal, [FromServices] AppDbContext context, int id) =>
+{
     Ong? ong = context.Ongs.Find(id);
 
     if (ong is null)
