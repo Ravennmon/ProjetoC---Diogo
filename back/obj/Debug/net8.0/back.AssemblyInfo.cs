43bf1a58af002d453c32da8f5b62141c37ec1c81
Pedro Dawybida <pedrodawybida@icloud.com>
2024-05-19 18:26:19 -0300

    fix - ajuste nas migrations

--- back/obj/Debug/net8.0/back.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a500a8388210a5ed6a31c2d8436a5edf045cc1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d77904af49ceee7aec3a12d4eb0ff8d9b9b2ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
