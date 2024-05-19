27669ffcd7a9f9aa7b210e471da474fd89bf3589
Henrique Onorato <onoratok2@gmail.com>
2024-05-19 13:57:38 -0300

    - criação ed validações

--- back/Models/Servico.cs
+++ back/Models/Servico.cs
@@ -1,6 +1,8 @@
 
 namespace back.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 public class Servico
 {
     public Servico(string nome, string descricao, int ongId)
@@ -11,8 +13,15 @@ public class Servico
     }
 
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório")]
+    [MinLength(3)]
+    [MaxLength(100)]
     public string Nome { get; set; }
-    public string Descricao { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string? Descricao { get; set; }
 
     public int OngId { get; set; }
     public Ong? Ong { get; set; }
