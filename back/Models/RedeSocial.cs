eb8e045951007db3b0471db256da785fbd49ce1f
Francyne Leocadio <francyneeleocadio@gmail.com>
2024-05-19 17:54:17 -0300

    feat - RedeSocial

---
+++ back/Models/RedeSocial.cs
@@ -0,0 +1,19 @@
+namespace back.Models;
+
+public class RedeSocial 
+{
+    public RedeSocial(string descricao, string url)  {
+        Url = url;
+        Descricao = descricao;
+    }
+
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string Descricao { get; set; }
+
+    public int? OngId { get; set; }
+    public Ong? Ong { get; set; }
+    
+    public int? TutorId { get; set; }
+    public Tutor? Tutor { get; set; }
+}
