257b6ac856a6e0c7bdc2efd9ee186c487f075770
Francyne Leocadio <francyneeleocadio@gmail.com>
2024-05-19 17:54:47 -0300

    feat - Tutor

---
+++ back/Models/Tutor.cs
@@ -0,0 +1,23 @@
+namespace back.Models;
+
+public class Tutor
+{
+    public Tutor(string nome, string email, string cpfCnpj, string telefone, string endereco, DateTime dataNascimento)  {
+        Nome = nome;
+        Email = email;
+        CpfCnpj = cpfCnpj;
+        Telefone = telefone;
+        Endereco = endereco;
+        DataNascimento = dataNascimento;
+    }
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string CpfCnpj { get; set; }
+    public string Telefone { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Endereco { get; set; }
+    public ICollection<RedeSocial>? RedesSociais { get; set; }
+    public ICollection<Animal>? Animais { get; set; }
+}
